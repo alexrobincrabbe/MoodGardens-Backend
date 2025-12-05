2b6a2c47a44fa6d5fd024fc744f310ab2bed3232
alexrobincrabbe <157045573+alexrobincrabbe@users.noreply.github.com>
2025-12-05 20:58:54 +0100

    update user public fields

--- src/modules/users/resolvers/const.ts
+++ src/modules/users/resolvers/const.ts
@@ -2,6 +2,8 @@ export const UserPublicFields = {
     id: true,
     email: true,
     isPremium: true,
+    premiumSince:true,
+    regenerateTokens:true,
     displayName: true,
     createdAt: true,
     timezone: true,
